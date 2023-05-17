e873d2d8a392eab5be99545f0230d2e0af16dc25
Kidong Nam <113520117+namkidong98@users.noreply.github.com>
2023-05-17 21:25:04 +0900

    Add files via upload

---
+++ phase2/src/ofApp.cpp
@@ -0,0 +1,496 @@
+/*
+
+	ofxWinMenu basic example - ofApp.cpp
+
+	Example of using ofxWinMenu addon to create a menu for a Microsoft Windows application.
+	
+	Copyright (C) 2016-2017 Lynn Jarvis.
+
+	https://github.com/leadedge
+
+	http://www.spout.zeal.co
+
+    =========================================================================
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    =========================================================================
+
+	03.11.16 - minor comment cleanup
+	21.02.17 - rebuild for OF 0.9.8
+
+*/
+#include "ofApp.h"
+#include <iostream>
+
+
+using namespace std;
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetWindowTitle("Maze Example"); // Set the app name on the title bar
+	ofSetFrameRate(15);
+	ofBackground(255, 255, 255);
+	// Get the window size for image loading
+	windowWidth = ofGetWidth();
+	windowHeight = ofGetHeight();
+	isdfs = false;
+	isOpen = 0;
+	// Centre on the screen
+	ofSetWindowPosition((ofGetScreenWidth()-windowWidth)/2, (ofGetScreenHeight()-windowHeight)/2);
+
+	// Load a font rather than the default
+	myFont.loadFont("verdana.ttf", 12, true, true);
+
+	// Load an image for the example
+	//myImage.loadImage("lighthouse.jpg");
+
+	// Window handle used for topmost function
+	hWnd = WindowFromDC(wglGetCurrentDC());
+
+	// Disable escape key exit so we can exit fullscreen with Escape (see keyPressed)
+	ofSetEscapeQuitsApp(false);
+
+	//
+	// Create a menu using ofxWinMenu
+	//
+
+	// A new menu object with a pointer to this class
+	menu = new ofxWinMenu(this, hWnd);
+
+	// Register an ofApp function that is called when a menu item is selected.
+	// The function can be called anything but must exist. 
+	// See the example "appMenuFunction".
+	menu->CreateMenuFunction(&ofApp::appMenuFunction);
+
+	// Create a window menu
+	HMENU hMenu = menu->CreateWindowMenu();
+
+	//
+	// Create a "File" popup menu
+	//
+	HMENU hPopup = menu->AddPopupMenu(hMenu, "File");
+
+	//
+	// Add popup items to the File menu
+	//
+
+	// Open an maze file
+	menu->AddPopupItem(hPopup, "Open", false, false); // Not checked and not auto-checked
+	
+	// Final File popup menu item is "Exit" - add a separator before it
+	menu->AddPopupSeparator(hPopup);
+	menu->AddPopupItem(hPopup, "Exit", false, false);
+
+	//
+	// View popup menu
+	//
+	hPopup = menu->AddPopupMenu(hMenu, "View");
+
+	bShowInfo = true;  // screen info display on
+	menu->AddPopupItem(hPopup, "Show DFS",false,false); // Checked
+	bTopmost = false; // app is topmost
+	menu->AddPopupItem(hPopup, "Show BFS"); // Not checked (default)
+	bFullscreen = false; // not fullscreen yet
+	menu->AddPopupItem(hPopup, "Full screen", false, false); // Not checked and not auto-check
+
+	//
+	// Help popup menu
+	//
+	hPopup = menu->AddPopupMenu(hMenu, "Help");
+	menu->AddPopupItem(hPopup, "About", false, false); // No auto check
+
+	// Set the menu to the window
+	menu->SetWindowMenu();
+
+} // end Setup
+
+
+//
+// Menu function
+//
+// This function is called by ofxWinMenu when an item is selected.
+// The the title and state can be checked for required action.
+// 
+void ofApp::appMenuFunction(string title, bool bChecked) {
+
+	ofFileDialogResult result;
+	string filePath;
+	size_t pos;
+
+	//
+	// File menu
+	//
+	if(title == "Open") {
+		readFile();
+	}
+	if(title == "Exit") {
+		if (isOpen) {
+			freeMemory();
+			cout << "Dynamically allocated memories freed" << endl;
+		}
+		ofExit(); // Quit the application
+	}
+
+	//
+	// Window menu
+	//
+	if(title == "Show DFS") {
+		//bShowInfo = bChecked;  // Flag is used elsewhere in Draw()
+		if (isOpen)
+		{
+			DFS();
+			bShowInfo = bChecked;
+		}
+		else
+			cout << "you must open file first" << endl;
+		
+	}
+
+	if(title == "Show BFS") {
+		doTopmost(bChecked); // Use the checked value directly
+
+	}
+
+	if(title == "Full screen") {
+		bFullscreen = !bFullscreen; // Not auto-checked and also used in the keyPressed function
+		doFullScreen(bFullscreen); // But als take action immediately
+	}
+
+	//
+	// Help menu
+	//
+	if(title == "About") {
+		ofSystemAlertDialog("ofxWinMenu\nbasic example\n\nhttp://spout.zeal.co");
+	}
+
+} // end appMenuFunction
+
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+}
+
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	char str[256];
+	//ofBackground(0, 0, 0, 0);
+	ofSetColor(100);
+	ofSetLineWidth(5);
+	int i, j; int number = 0;
+	int x = 30, y = 5; //starting point
+	int line_length = 35;
+	
+	// TO DO : DRAW MAZE; 
+	// 저장된 자료구조를 이용해 미로를 그린다.
+	if (isOpen) {
+		for (i = 0; i < height_graph; i++) {
+			for (j = 0; j < width_graph; j++) {
+				if (graph[number]->link[0] == NULL) ofDrawLine(x, y, x + line_length, y);
+				if (graph[number]->link[1] == NULL) ofDrawLine(x + line_length, y, x + line_length, y + line_length);
+				if (graph[number]->link[2] == NULL) ofDrawLine(x, y + line_length, x + line_length, y + line_length);
+				if (graph[number]->link[3] == NULL) ofDrawLine(x, y, x, y + line_length);
+				x += line_length;
+				number++;
+			}
+			x = 30; //leftmost(starting point)
+			y += line_length;
+		}
+
+
+	}
+
+
+	if (isdfs)
+	{
+		ofSetColor(200);
+		ofSetLineWidth(5);
+		if (isOpen)
+			dfsdraw();
+		else
+			cout << "You must open file first" << endl;
+	}
+	if(bShowInfo) {
+		// Show keyboard duplicates of menu functions
+		sprintf(str, " comsil project");
+		myFont.drawString(str, 15, ofGetHeight()-20);
+	}
+
+} // end Draw
+
+
+void ofApp::doFullScreen(bool bFull)
+{
+	// Enter full screen
+	if(bFull) {
+		// Remove the menu but don't destroy it
+		menu->RemoveWindowMenu();
+		// hide the cursor
+		ofHideCursor();
+		// Set full screen
+		ofSetFullscreen(true);
+	}
+	else { 
+		// return from full screen
+		ofSetFullscreen(false);
+		// Restore the menu
+		menu->SetWindowMenu();
+		// Restore the window size allowing for the menu
+		ofSetWindowShape(windowWidth, windowHeight + GetSystemMetrics(SM_CYMENU)); 
+		// Centre on the screen
+		ofSetWindowPosition((ofGetScreenWidth()-ofGetWidth())/2, (ofGetScreenHeight()-ofGetHeight())/2);
+		// Show the cursor again
+		ofShowCursor();
+		// Restore topmost state
+		if(bTopmost) doTopmost(true);
+	}
+
+} // end doFullScreen
+
+
+void ofApp::doTopmost(bool bTop)
+{
+	if(bTop) {
+		// get the current top window for return
+		hWndForeground = GetForegroundWindow();
+		// Set this window topmost
+		SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE); 
+		ShowWindow(hWnd, SW_SHOW);
+	}
+	else {
+		SetWindowPos(hWnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+		ShowWindow(hWnd, SW_SHOW);
+		// Reset the window that was topmost before
+		if(GetWindowLong(hWndForeground, GWL_EXSTYLE) & WS_EX_TOPMOST)
+			SetWindowPos(hWndForeground, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE); 
+		else
+			SetWindowPos(hWndForeground, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE); 
+	}
+} // end doTopmost
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+	
+	// Escape key exit has been disabled but it can be checked here
+	if(key == VK_ESCAPE) {
+		// Disable fullscreen set, otherwise quit the application as usual
+		if(bFullscreen) {
+			bFullscreen = false;
+			doFullScreen(false);
+		}
+		else {
+			ofExit();
+		}
+	}
+
+	// Remove or show screen info
+	if(key == ' ') {
+		bShowInfo = !bShowInfo;
+		// Update the menu check mark because the item state has been changed here
+		menu->SetPopupItem("Show DFS", bShowInfo);
+	}
+
+	if(key == 'f') {
+		bFullscreen = !bFullscreen;	
+		doFullScreen(bFullscreen);
+		// Do not check this menu item
+		// If there is no menu when you call the SetPopupItem function it will crash
+	}
+
+} // end keyPressed
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+bool ofApp::readFile()
+{
+	int i, j, command;
+	ofFileDialogResult openFileResult = ofSystemLoadDialog("Select .maz file");
+	string filePath;
+	size_t pos;
+	// Check whether the user opened a file
+	if (openFileResult.bSuccess) {
+		ofLogVerbose("User selected a file");
+
+		//We have a file, check it and process it
+		string fileName = openFileResult.getName();
+		//string fileName = "maze0.maz";
+		cout << "file name is " << fileName << endl;
+		//printf("file name is %s\n", fileName);
+		filePath = openFileResult.getPath();
+		printf("Open\n");
+		pos = filePath.find_last_of(".");
+		if (pos != string::npos && pos != 0 && filePath.substr(pos + 1) == "maz") {
+
+			ofFile file(fileName);
+
+			if (!file.exists()) {
+				cout << "Target file does not exists." << endl;
+				return false;
+			}
+			else {
+				cout << "We found the target file." << endl;
+				isOpen = 1;
+			}
+
+			ofBuffer buffer(file);
+
+			for (ofBuffer::Line it = buffer.getLines().begin(), end = buffer.getLines().end(); it != end; ++it) {
+				HEIGHT++;
+				string line = *it;
+				WIDTH = line.size();
+			}
+
+			//int형 2차원 배열 maze를 동적 할당
+			maze = new int* [HEIGHT];
+			for (i = 0; i < HEIGHT; i++) {
+				maze[i] = new int[WIDTH];
+			}
+			i = 0;
+			
+			cout << "HEIGHT: " << HEIGHT << "WIDTH: " << WIDTH << endl;
+
+			//파일의 내용을 읽어서 maze에 저장
+			for (ofBuffer::Line it = buffer.getLines().begin(), end = buffer.getLines().end(); it != end; ++it, i++) {
+				string line = *it;
+				for (j = 0; j < WIDTH; j++) {
+					if (line[j] == '+') maze[i][j] = 1;
+					else if (line[j] == '-') maze[i][j] = 2;
+					else if (line[j] == '|') maze[i][j] = 3;
+					else if ((i % 2 == 1) && (j % 2 == 0) && line[j] == ' ') maze[i][j] = 4; //없어진 세로벽(높이는 홀수, 너비는 짝수)
+					else if ((i % 2 == 0) && (j % 2 == 1) && line[j] == ' ') maze[i][j] = 4; //없어진 가로벽(높이는 짝수, 너비는 홀수)
+					else maze[i][j] = 0;
+				}
+			}
+			
+			//for debugging
+			for (i = 0; i < HEIGHT; i++) {
+				for (j = 0; j < WIDTH; j++) {
+					command = maze[i][j];
+					switch (command) {
+					case 1:
+						cout << "+"; break;
+					case 2:
+						cout << "-"; break;
+					case 3:
+						cout << "|"; break;
+					case 4:
+						cout << " "; break;
+					default:
+						cout << " ";
+					}
+				}
+				cout << endl;
+			}
+
+			//graph를 만들기 위해 필요한 선작업
+			int number = 0;
+			height_graph = HEIGHT / 2;
+			width_graph = WIDTH / 2;
+			num_of_terms = height_graph * width_graph;
+			graph = new node_pointer[num_of_terms];
+
+			//maze를 읽어서 새로운 자료구조인 graph의 모든 vertex를 구성 및 초기화
+			for (i = 0; i < HEIGHT; i++) {
+				for (j = 0; j < WIDTH; j++) {
+					if ((i % 2 == 1) && (j % 2 == 1)) { //둘다 홀수인 경우가 maze의 방에 해당한다
+						node_pointer temp = new node;
+						temp->row = i; temp->col = j; //새로만든 node의 기본 정보 초기화
+						temp->index = number; 
+						temp->link[0] = NULL; temp->link[1] = NULL;
+						temp->link[2] = NULL; temp->link[3] = NULL;
+						graph[number++] = temp;
+					}
+				}
+			}
+
+			//초기화된 graph의 vertex를 비교하면서 edge를 연결
+			number = 0;
+			for (i = 0; i < num_of_terms; i++) {
+				node_pointer ptr = graph[number];
+				if (maze[ptr->row - 1][ptr->col] == 4) ptr->link[0] = graph[ptr->index - width_graph]; //위의 벽이 뚫려있으면
+				if (maze[ptr->row][ptr->col + 1] == 4) ptr->link[1] = graph[ptr->index + 1]; //우측 벽이 뚫려있으면
+				if (maze[ptr->row + 1][ptr->col] == 4) ptr->link[2] = graph[ptr->index + width_graph]; //아래 벽이 뚫려있으면
+				if (maze[ptr->row][ptr->col - 1] == 4) ptr->link[3] = graph[ptr->index - 1]; //좌측 벽이 뚫려있으면
+				number++;
+			}
+			
+		}
+		else {
+			printf("  Needs a '.maz' extension\n");
+			return false;
+		}
+	}
+}
+void ofApp::freeMemory() {
+	int i;
+	for (i = 0; i < HEIGHT; i++) {
+		delete[] maze[i]; 
+	}
+	delete[] maze; //2차원 배열
+
+	delete[] graph; //1차원 배열
+	
+}
+
+bool ofApp::DFS()//DFS탐색을 하는 함수
+{
+	//TO DO
+	//DFS탐색을 하는 함수 ( 3주차)
+	return true;
+}
+void ofApp::dfsdraw()
+{
+	//TO DO 
+	//DFS를 수행한 결과를 그린다. (3주차 내용)
+}
+
